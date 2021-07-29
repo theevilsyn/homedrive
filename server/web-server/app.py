759a6983586d6b943977364de660bb1b47b1fbcd
Jaswanth Bommidi <jassu.bommidi@gmail.com>
2021-07-30 02:28:00 +0530

    Add Server Prototype

--- server/web-server/app.py
+++ server/web-server/app.py
@@ -1,10 +1,97 @@
-from time import sleep
-from flask import Flask
+import datetime
+import uuid
+from functools import wraps
+from os import environ
+
+import jwt
+from flask import Flask, jsonify, make_response, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "welcome to the flask tutorials"
+app.config['SECRET_KEY'] = 'thisissecret'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/f4lc0n/Documents/CTFs/InCTFi21/Pentest/homedrive/server/todo.db'
 
-if __name__ == "__main__":
-	app.run(host ='0.0.0.0', port = 5001, debug = True)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
+
+db.create_all()
+
+def token_required(function):
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(
+                public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return function(current_user, *args, **kwargs)
+    return decorated
+
+
+@app.route('/register', methods=['POST'])
+def create_user():
+    data = request.get_json(force=True)
+
+    email = User.query.filter_by(email=data['email']).first()
+    uname = User.query.filter_by(username=data['username']).first()
+
+    if uname or email:
+        return jsonify({"message": "Username or email already in use!"})
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(public_id=str(uuid.uuid4()),
+                    name=data['username'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created!'})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.get_json(force=True)
+
+    if not auth or not auth['username'] or not auth['password']:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(name=auth['username']).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user['username'], auth['password']):
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
